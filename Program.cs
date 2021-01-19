6ea36ffa67ee10e6746b9db5f209a73270c35b07
EHesenov2000 <elmanhsnov06@gmail.com>
2021-01-19 10:37:06 +0400

    Your car's fuel

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FuelQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // commmon fuel  -  l
+            // way length    -  m
+            // fuel for 1 km -  k
+            int l = 100; 
+            double k = 1.5;
+            int m = 100;
+
+            if (l > 0 && k > 0 && m > 0)
+            {
+                if (l > m * k)
+                {
+                    Console.WriteLine("You can go this distance with your car's fuel");
+
+                }
+                else
+                {
+                    Console.WriteLine("You can not go this distance with your car's fuel");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please, enter correct numbers...");
+
+            }
+        }
+    }
+}
